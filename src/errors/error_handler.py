57a2ce4fb5dca619f45ef9b75c3519384edce65c
Lucas Lopes da Silva <okasanjelucas@gmail.com>
2024-02-11 01:22:20 -0300

    feat: Implementing validator

--- src/errors/error_handler.py
+++ src/errors/error_handler.py
@@ -1,6 +1,21 @@
 from src.views.http_types.http_response import HttpResponse
+from .error_types.http_unprocessable_entity import HttpUnprocessableEntityError
 
 def handle_errors(error: Exception) -> HttpResponse:
+    if isinstance(error, HttpUnprocessableEntityError):
+
+        # If needed, we can apply more changes in here, like report the error on a e-mail, etc.
+
+        return HttpResponse(
+            status_code=error.status_code,
+            body={
+                "errors": [ {
+                    "title": error.name,
+                    "details": error.message,
+                }]
+            }
+        )
+
     return HttpResponse(
         status_code=500,
         body={
